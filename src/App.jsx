b5ebbc30d6e340c0f629dd712eb17d3c634b4442
walid hasan <walid.official8@gmail.com>
2024-10-20 14:53:55 +0600

    project-setup is done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './components/Countries'
+function App() {
+
+  return (
+    <>
+      <h1>Rest Country</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Countries.jsx
@@ -0,0 +1,10 @@
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Content Here</h2>
+        </div>
+    );
+};
+
+export default Countries;
